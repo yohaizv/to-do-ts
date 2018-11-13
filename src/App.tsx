349393435c4b93c4341eb16ea3dbb25ee2803538
unknown <yohaizv@gmail.com>
2018-11-13 10:06:20 +0200

    persist removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
 import "mimic";
 import * as React from "react";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import styled from "styled-components";
 import "./App.css";
 import Header from "./components/Header";
 import Dashboard from "./scenes/Dashboard";
-import configureStore from "./store";
-
-const storeCreator = configureStore();
+import store from "./store";
 
 const StyledPageBody = styled.div`
   max-width: 600px;
-  margin:auto;
+  margin: auto;
 `;
 
 const App = () => {
   return (
-    <Provider store={storeCreator.store}>
-      <PersistGate loading={null} persistor={storeCreator.persistor}>
+    <Provider store={store}>
+      <div>
         <Header title="TODO LIST" />
         <StyledPageBody>
           <Dashboard />
         </StyledPageBody>
-      </PersistGate>
+      </div>
     </Provider>
   );
 };
